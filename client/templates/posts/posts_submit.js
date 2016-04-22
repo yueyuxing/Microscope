0a29b8b9f6e425d0e44e6774ae06c66739fb1700
yueyuxing <45320539@qq.com>
2016-04-23 00:30:21 +0800

    强制帖子的唯一性

--- client/templates/posts/posts_submit.js
+++ client/templates/posts/posts_submit.js
@@ -9,9 +9,14 @@ Template.postSubmit.events({
 
     Meteor.call('postInsert', post, function(error, result) {
       // Meteor.call 方法通过第一个参数来调用其方法。
+      
       // 显示错误信息并退出
       if (error)
         return alert(error.reason);
+      // 显示结果，跳转页面
+      if (result.postExists)
+        alert('This link has already been posted（该链接已经存在）');
+
       Router.go('postPage', {_id: result._id});
     });
   }
