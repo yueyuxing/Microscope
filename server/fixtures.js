ac29f76580775f4f678e89ad2121e2cff68a4317
yueyuxing <45320539@qq.com>
2016-04-22 23:09:44 +0800

    添加posts集合加入数据，连接到postsList模板上

---
+++ server/fixtures.js
@@ -0,0 +1,16 @@
+if (Posts.find().count() === 0) { // 如果为空则载入三条帖子
+  Posts.insert({
+    title: 'Introducing Telescope',
+    url: 'http://sachagreif.com/introducing-telescope/'
+  });
+
+  Posts.insert({
+    title: 'Meteor',
+    url: 'http://meteor.com'
+  });
+
+  Posts.insert({
+    title: 'The Meteor Book',
+    url: 'http://themeteorbook.com'
+  });
+}
