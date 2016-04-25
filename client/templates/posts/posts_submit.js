a2ed3a779c3025fc6c51b104dcc9e6abc506e984
yueyuxing <45320539@qq.com>
2016-04-25 20:51:34 +0800

    实际使用错误报告

--- client/templates/posts/posts_submit.js
+++ client/templates/posts/posts_submit.js
@@ -12,10 +12,10 @@ Template.postSubmit.events({
 
       // 显示错误信息并退出
       if (error)
-        return alert(error.reason);
+        return throwError(error.reason);
       // 显示结果，跳转页面
       if (result.postExists)
-        alert('This link has already been posted（该链接已经存在）');
+        throwError('This link has already been posted（该链接已经存在）');
 
       Router.go('postPage', {_id: result._id});
     });
